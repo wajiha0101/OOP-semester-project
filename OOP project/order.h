23ca1bf1c82b1e2c13f83a3d4d014f2fb2e4eb15
wajiha0101 <149070831+wajiha0101@users.noreply.github.com>
2024-12-09 15:15:09 +0500

    Update order.h

--- OOP project/order.h
+++ OOP project/order.h
@@ -21,6 +21,9 @@ public:
 			cout << "Your cart is empty\n";
 			return;
 		}
+		saveorder("order.h");
+		cout << "Order placed successfully!\n";
+		displayOrderDetails();
 	}
 	void checkoutinfo() {
 		cout << "Customers Details\n";
@@ -35,6 +38,37 @@ public:
 		}
 		cout << "Total price: " << totalprice << endl;
 	}
+	void saveorder(const string& orderpath) {
+		ofstream orderfile(orderpath);
+		if (!orderfile.is_open()) {
+			cout << "File not found\n";
+			return;
+		}
+		orderfile << "Customer Details:\n";     //userinfo saving
+		orderfile << "Name: " << u.name << "\n";
+		orderfile << "Email: " << u.email << "\n";
+		orderfile << "Address: " << u.address << "\n";
+		orderfile << "Phone: " << u.phone << "\n";
+		//save cart items
+		for (const auto& oderitem : c.getcartitems()) {
+			orderfile << oderitem.id << oderitem.name<<oderitem.price<<"\n";
+		}
+		orderfile << "Total Price: $" << c.gettotalprice() << "\n";
+		orderfile << "--------------------------\n";
+		orderfile.close();
+	}
+	
+	void displayAllOrders() {
+		if (orderitems.empty()) {
+			cout << "No orders found.\n";
+			return;
+		}
 
+		cout << "Displaying all orders:\n";
+		for (const auto& cart : orderitems) {
+			cart.displaycart();
+			cout << "Total Price: $" << cart.gettotalprice() << endl;
+		}
+	}
 
 };
